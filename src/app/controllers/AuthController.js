f9a2b52499547d5b4b46e7c1ae8c95ffad20d2a5
hoang <hoangyl245@gmail.com>
2022-07-10 09:02:22 +0700

    fixk

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -4,10 +4,10 @@ const {MongooseToObject, mutipleMongooseToObject} = require('../../util/mongoose
 
 class AuthController{
     login(req,res,next){
-        res.render("auth/login",{layout:false});
+        res.render("auth/login");
     }
     signup(req,res,next){
-        res.render("auth/signup",{layout:false});
+        res.render("auth/signup");
     }
 }
 
