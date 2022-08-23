48dcda222fb7e2784b8d3915b93991e9eec7808b
hoang <hoangyl245@gmail.com>
2022-08-23 22:59:10 +0700

    o

--- src/routes/me.js
+++ src/routes/me.js
@@ -1,15 +1,15 @@
 
-// cấu hình courseController
+// // cấu hình courseController
 
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-const meController = require('../app/controllers/MeController');
+// const meController = require('../app/controllers/MeController');
 
-//newcontroller.index
-router.get('/trash/items', meController.trashCourse);
+// //newcontroller.index
+// router.get('/trash/items', meController.trashCourse);
 
-router.get('/stored/items', meController.storedCourses);
+// router.get('/stored/items', meController.storedCourses);
 
-module.exports = router;
+// module.exports = router;
 
